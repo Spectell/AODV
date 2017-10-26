1d8f181cb52794190828bca1d64b52f728ae01aa
tochi <30542456+Spectell@users.noreply.github.com>
2017-10-26 17:34:07 +0900

    Update AODV.m

--- AODV.m
+++ AODV.m
@@ -1,42 +1,41 @@
-% 作りかけ
+%%% Sorry. This program-code is unfinished.
 clear
 
-%%% 可変領域
-L = 10;         % はんい
-NoN = 20;       % ノード数
-R = 2; % すべてのnodeが同じ能力の場合
-       % ノードの最大通信範囲で座標ボードを大きく
+%%% Rewritable
+L = 10;         % length
+NoN = 20;       %　No. of node 
+R = 2; % Submit range node
 %%%
 
-A = zeros(L+R+R);  %%%　ノードの位置を座標で表現＿①
+A = zeros(L+R+R);  %%% node's position__(1)
 
-Node_line = randi([1,L],[1,NoN])+R; %%% ノードの座標を作成
+Node_line = randi([1,L],[1,NoN])+R; %%% make node's position
 Node_row  = randi([1,L],[1,NoN])+R;
 
 for i = 1:NoN
-    %%% (i)に当たるところで、nodeを表す
-    S_Node(i).line = Node_line(i);                %%% Nodeの位置
+    %%% status of node
+    S_Node(i).line = Node_line(i);                %%% node's position
     S_Node(i).row  = Node_row(i); 
-    S_Node(i).RREQ = [];                 %%% どの順番でRREQが送られてきたのか ,はじめは0を入れておきます
-    S_Node(i).range = [R];                        %%% Node の　通信はんい | ここを操作して　機器ごとの通信範囲を変える
+    S_Node(i).RREQ = [];                          %%% memory of RREQ
+    S_Node(i).range = [R];                        %%% Submit range node
     %%%     
 
-    A(Node_line(i),Node_row(i)) = i;   %%%　ノードの位置を座標で表現＿②
+    A(Node_line(i),Node_row(i)) = i;   %%% write node's position__(2)
 end
 
-spy(A);  %%% ノードの位置を座標で表現＿③
-flag = 0;  % はじめ　node1 からnode20 へ
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  あとで　findで書き直せるか試す
-k = 1;
+spy(A);    %%% display node's position__(3)
+flag = 0;  % using test
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% rewrite by using find ?
+k = 1;     % start at node-1
 
-for roop = 1:6  %　取りあえず、６回してるだけ
+for roop = 1:6  % Here is test. if here is complete--->> while(1)
    
 for i = 1 : S_Node(k(1)).range
-    %%% Aを使って参照し,S_Nodeにアクセスすることで実現する
+        %%% send RREQ
+        %%% access S_Node.RREQ by using (A:node's position__(4))
     for j = i : S_Node(k(1)).range
-        
       if A( S_Node(k(1)).line +i , S_Node(k(1)).row +j ) > 0 
-      %%% ４つとも、ここに条件を追加する必要あり
+      %%% need adding requirement
      S_Node(A( S_Node(k(1)).line +i , S_Node(k(1)).row +j )).RREQ = ...
          [S_Node(A( S_Node(k(1)).line +i , S_Node(k(1)).row +j )).RREQ k(1)];
      k = [k A( S_Node(k(1)).line +i , S_Node(k(1)).row +j )];
@@ -65,8 +64,8 @@ for i = 1 : S_Node(k(1)).range
     end            
 end
 k(1)=[];
-    if S_Node(20).RREQ > 0
-      flag = flag +1; %%%　　おわり
+    if S_Node(NoN).RREQ > 0
+      flag = flag +1; %%%　last node
     end
 
     if isempty(k)==1 
